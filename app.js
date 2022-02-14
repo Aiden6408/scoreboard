945a69c93f8daad3b181787a6cba59ccd918a42c
Aiden Eskatakii <aidenjameseskatakii@gmail.com>
2022-02-13 23:11:10 -0700

    ooooopppppppppssssss

---
+++ app.js
@@ -0,0 +1,24 @@
+let team1 = 0
+let team2 = 0
+
+
+
+
+function upscore(team1, team2) {
+    team1 += 1
+    team2 += 1
+
+    document.getElementById('team1').innerHTML = team1
+    console.log()
+
+    document.getElementById('team2').innerHTML = team2
+}
+
+
+
+
+
+
+
+
+
